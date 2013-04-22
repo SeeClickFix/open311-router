e0218e0a9dee94d146cc0bb9bb5ca390f462fae5
Kam Lasater <ckl@seekayel.com>
2013-04-22 08:57:34 -0700

    Init of rails3 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Open311Router::Application.config.secret_token = '9c42d66313ed99041122421900fdefe71218e2620cd3940e73c876b598c6963ff3361bed0a004c79b714a6c97e01811043c158f206674dbf0ac164757f05230f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Open311Router::Application.config.session_store :cookie_store, key: '_open311-router_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Open311Router::Application.config.session_store :active_record_store
