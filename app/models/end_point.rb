c5eefdf213ef2a0981f7c2e63e2f8704b01f5383
Kam Lasater <ckl@seekayel.com>
2013-05-03 16:07:09 -0400

    Added json and xml rendering with removal of wrong attributes

--- app/models/end_point.rb
+++ app/models/end_point.rb
@@ -1,3 +1,14 @@
 class EndPoint < ActiveRecord::Base
   attr_accessible :change_set, :environment, :specification, :url
+  
+  Except_List = [:created_at, :updated_at, :server_id, :id]
+  
+  def as_json(options={})
+    super(:except => Except_List)
+  end
+  
+  def to_xml(options={})
+    options.merge!(:except => Except_List)
+    super(options)
+  end
 end
